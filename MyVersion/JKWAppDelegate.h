15df5f7296de95afbd71c8b58d9a10a1b9c50b69
Jennifer K Waldner <jenniferkwaldner@gmail.com>
2014-05-29 18:10:23 -0400

    Initial Commit

---
+++ MyVersion/JKWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKWAppDelegate.h
+//  MyVersion
+//
+//  Created by Jennifer Waldner on 5/29/14.
+//  Copyright (c) 2014 Jennifer K Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyVersion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyVersion
+//
+//  Created by Jennifer Waldner on 5/29/14.
+//  Copyright (c) 2014 Jennifer K Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKWAppDelegate class]));
+    }
+}
